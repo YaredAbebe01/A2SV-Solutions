1a58ac64a657d24583b882bec4e0ca33948a2791
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2024-07-06 11:51:18 +0300

    Create Two Sum II - Input Array Is Sorted.py

---
+++ Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left, right = 0, len(numbers) - 1
+        
+        while left < right:
+            csum = numbers[left] + numbers[right]
+            if csum == target:
+                return [left + 1, right + 1]
+            elif csum < target:
+                left += 1
+            else:
+                right -= 1
+        
+        return [-1, -1]
+        
